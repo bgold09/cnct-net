47394d416485349043ab4c85deb186d32217ace4
Brian Golden <bgold09@users.noreply.github.com>
2019-04-19 23:16:04 -0700

    deser

---
+++ Cnct/Cnct.Core.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+using Cnct.Core;
+using Newtonsoft.Json;
+
+namespace Cnct.Core.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            string json = System.IO.File.ReadAllText(@"C:\Users\brian\.dotfiles\cnct.json");
+
+            var config = JsonConvert.DeserializeObject<CnctConfig>(json);
+
+            Console.WriteLine();
+        }
+    }
+}
--- Cnct/Cnct.Core/Class1.cs
+++ Cnct/Cnct.Core/Class1.cs
@@ -4,5 +4,18 @@ namespace Cnct.Core
 {
     public class Class1
     {
+        public int Num;
+
+        public void T(object value)
+        {
+            switch (value)
+            {
+                case Class1 c:
+                    break;
+
+                default:
+                    break;
+            }
+        }
     }
 }
