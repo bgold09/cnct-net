e8ba871feafb5dee70ee3c66d0613810a52c2d51
Brian Golden <bgold09@users.noreply.github.com>
2019-05-03 12:41:43 -0400

    fix test

--- Cnct/Cnct.Core.Tests/LinkTaskSpecificationTests.cs
+++ Cnct/Cnct.Core.Tests/LinkTaskSpecificationTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Cnct.Core.Configuration;
 using Xunit;
@@ -10,8 +11,11 @@ namespace Cnct.Core.Tests
         [Fact]
         public void ExplicitLinkPath()
         {
+            string configRootDirectory = "test";
             string target = "file.ext";
-            string explicitLinkPath = "~/some/pathrc";
+            string expectedFullTargetPath = $"{configRootDirectory}{Path.DirectorySeparatorChar}{target}";
+
+            string explicitLinkPath = $"{Path.DirectorySeparatorChar}some{Path.DirectorySeparatorChar}path";
 
             var linkSpec = new LinkTaskSpecification()
             {
@@ -21,12 +25,12 @@ namespace Cnct.Core.Tests
                 }
             };
 
-            var actualLinks = linkSpec.GetLinkConfigurations();
+            var actualLinks = linkSpec.GetLinkConfigurations(configRootDirectory);
 
             Assert.Equal(1, actualLinks.Count);
-            Assert.Contains(target, actualLinks);
+            Assert.Contains(expectedFullTargetPath, actualLinks);
 
-            IEnumerable<string> links = actualLinks[target];
+            IEnumerable<string> links = actualLinks[expectedFullTargetPath];
             Assert.Single(links);
             Assert.Equal(explicitLinkPath, links.Single());
         }
