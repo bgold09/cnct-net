94f029b479b2dcc451410770c359c517d548d8ce
Brian Golden <bgold09@users.noreply.github.com>
2020-05-06 19:11:47 -0700

    wip: exec

---
+++ Cnct/Cnct.Core/Tasks/ShellTask.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cnct.Core.Tasks
+{
+    internal class ShellTask : CnctTaskBase
+    {
+        public ShellTask(ILogger logger)
+            : base(logger)
+        {
+        }
+
+        public override Task ExecuteAsync()
+        {
+            var startInfo = new ProcessStartInfo()
+            {
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = true,
+                //FileName ="" ,
+                //Arguments = ""
+            };
+
+            using var process = new Process()
+            {
+                StartInfo = startInfo,
+            };
+;
+            var outputs = new List<string>();
+            var errors = new List<string>();
+
+            process.OutputDataReceived += new DataReceivedEventHandler((sender, e) =>
+            {
+                outputs.Add(e.Data);
+                Console.WriteLine(e.Data);
+            });
+
+            process.ErrorDataReceived += new DataReceivedEventHandler((sender, e) =>
+            {
+                errors.Add(e.Data);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e.Data);
+                Console.ResetColor();
+            });
+
+            //PowerShell
+
+            //process.StandardError
+
+            return Task.CompletedTask;
+        }
+    }
+}
