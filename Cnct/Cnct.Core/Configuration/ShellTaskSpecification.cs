cb3c0b1ebfb4954114efae8f70c9b198a2978b63
Brian Golden <bgold09@users.noreply.github.com>
2020-05-08 11:32:58 -0700

    add a working shell task for powershell

---
+++ Cnct/Cnct.Core/Configuration/ShellTaskSpecification.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cnct.Core.Tasks.Shell;
+using Newtonsoft.Json;
+
+namespace Cnct.Core.Configuration
+{
+    public class ShellTaskSpecification : ICnctActionSpec
+    {
+        public string ActionType { get; } = "shell";
+
+        [JsonRequired]
+        public ShellType Shell { get; set; }
+
+        [JsonRequired]
+        public string Command { get; set; }
+
+        [JsonProperty("os")]
+        [JsonRequired]
+        [JsonConverter(typeof(LinkCollectionConverter))]
+        public string[] PlatformType { get; set; }
+
+        public bool Silent { get; set; }
+
+        private readonly HashSet<PlatformType> platformTypes = new HashSet<PlatformType>();
+
+        public async Task ExecuteAsync(ILogger logger, string configDirectoryRoot)
+        {
+            if (!this.platformTypes.Contains(Platform.CurrentPlatform))
+            {
+                return;
+            }
+
+            IShellInvoker shellInvoker = this.Shell switch
+            {
+                ShellType.PowerShell => new PowerShellInvoker(),
+                _ => throw new IndexOutOfRangeException(),
+            };
+
+            await shellInvoker.ExecuteAsync(this);
+        }
+
+        public void Validate()
+        {
+            if (this.Shell == ShellType.Unknown)
+            {
+                throw new ArgumentOutOfRangeException($"Shell type not recognized.");
+            }
+
+            if (string.IsNullOrWhiteSpace(this.Command))
+            {
+                throw new ArgumentException("A command must be specified.");
+            }
+
+            if (!this.PlatformType.Any())
+            {
+                throw new ArgumentException("At least one OS must be specified.");
+            }
+
+            foreach (string item in this.PlatformType)
+            {
+                if (!Enum.TryParse(item, true, out PlatformType platformType))
+                {
+                    throw new ArgumentOutOfRangeException($"OS type '{item}' was not recognized.");
+                }
+
+                this.platformTypes.Add(platformType);
+            }
+        }
+
+        public enum ShellType
+        {
+            Unknown = 0,
+            PowerShell,
+        }
+    }
+}
---
+++ Cnct/Cnct.Core/Tasks/Shell/PowerShellInvoker.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Threading.Tasks;
+using Cnct.Core.Configuration;
+
+namespace Cnct.Core.Tasks.Shell
+{
+    public class PowerShellInvoker : IShellInvoker
+    {
+        private static readonly InitialSessionState Iss = InitialSessionState.CreateDefault();
+
+        public async Task ExecuteAsync(ShellTaskSpecification specification)
+        {
+            if (specification == null)
+            {
+                throw new ArgumentNullException(nameof(specification));
+            }
+
+            // need this to be configurable
+            Iss.ExecutionPolicy = Microsoft.PowerShell.ExecutionPolicy.Unrestricted;
+
+            using var powershell = PowerShell.Create(Iss);
+            powershell.AddScript(specification.Command);
+
+            powershell.Streams.Error.DataAdded += ToStandardError<ErrorRecord>;
+            powershell.Streams.Warning.DataAdded += ToStandardOutput<WarningRecord>;
+            if (!specification.Silent)
+            {
+                powershell.Streams.Information.DataAdded += ToStandardOutput<InformationRecord>;
+            }
+
+            await powershell.InvokeAsync();
+            if (powershell.HadErrors)
+            {
+                throw new InvalidOperationException("command failed");
+            }
+        }
+
+        private static void ToStandardError<T>(object sender, DataAddedEventArgs args)
+        {
+            ToStream<T>(sender, args, Console.Error);
+        }
+
+        private static void ToStandardOutput<T>(object sender, DataAddedEventArgs args)
+        {
+            ToStream<T>(sender, args, Console.Out);
+        }
+
+        private static void ToStream<T>(object sender, DataAddedEventArgs args, TextWriter writer)
+        {
+            if (!(sender is PSDataCollection<T> collection))
+            {
+                throw new InvalidOperationException();
+            }
+
+            writer.WriteLine(collection[args.Index]);
+        }
+    }
+}
